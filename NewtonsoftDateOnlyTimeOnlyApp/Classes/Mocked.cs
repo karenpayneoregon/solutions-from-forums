3d6e6de0df4452b38c20baca03b59c4706d69cba
Karen Payne <paynekaren@comcast.net>
2022-12-01 09:39:29 -0800

    moved class

---
+++ NewtonsoftDateOnlyTimeOnlyApp/Classes/Mocked.cs
@@ -0,0 +1,27 @@
+﻿using NewtonsoftDateOnlyTimeOnlyApp.Models;
+
+namespace NewtonsoftDateOnlyTimeOnlyApp.Classes;
+
+public class Mocked
+{
+    public static List<Container> Container() =>
+        new()
+        {
+            new()
+            {
+                Id = 1,
+                FirstName = "Karen",
+                LastName = "Payne",
+                StartDate = new DateOnly(2022,12,1),
+                StartTime = new TimeOnly(14,15)
+            },
+            new()
+            {
+                Id = 2,
+                FirstName = "May",
+                LastName = "Gallagher",
+                StartDate = new DateOnly(2022,12,11),
+                StartTime = new TimeOnly(16,0)
+            }
+        };
+}
--- NewtonsoftDateOnlyTimeOnlyApp/Models/Container.cs
+++ NewtonsoftDateOnlyTimeOnlyApp/Models/Container.cs
@@ -9,27 +9,3 @@ public class Container
     public DateOnly StartDate { get; set; }
     public TimeOnly StartTime { get; set; }
 }
-
-public class Mocked
-{
-    public  static List<Container> Container() =>
-        new()
-        {
-            new()
-            {
-                Id = 1, 
-                FirstName = "Karen", 
-                LastName = "Payne", 
-                StartDate = new DateOnly(2022,12,1), 
-                StartTime = new TimeOnly(14,15)
-            },
-            new()
-            {
-                Id = 2, 
-                FirstName = "May", 
-                LastName = "Gallagher", 
-                StartDate = new DateOnly(2022,12,11), 
-                StartTime = new TimeOnly(16,0)
-            }
-        };
-}
