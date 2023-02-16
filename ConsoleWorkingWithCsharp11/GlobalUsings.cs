7c481a3a1f85da91a43497b6f3d63a866e269e89
Karen Payne <paynekaren@comcast.net>
2023-02-16 07:18:52 -0800

    removed several projects

--- ConsoleWorkingWithCsharp11/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-// Global using directives
-
-global using ConsoleWorkingWithCsharp11.Data;
-global using ConsoleWorkingWithCsharp11.Models;
-global using Spectre.Console;
---
+++ RadioButtonsExample/Pages/RadioButton1.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RadioButtonsExample.Models;
+using System.Reflection;
+using RadioButtonsExample.Classes;
+
+namespace RadioButtonsExample.Pages
+{
+    public class RadioButton1Model : PageModel
+    {
+     
+        public List<Shape> Shapes { get; set; } = Operations.Shapes;
+        [BindProperty]
+        public Shape Shape { get; set; }
+
+
+        public void OnGet()
+        {
+            Shape = Shapes[1];
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            await Task.Delay(0);
+
+            var selection = Shape;
+            return Page();
+        }
+    }
+}
